a15d008af1a3bc3b27c2ac098fec826864e6b07f
juliano <juliano@2fa30b31-ecef-0310-9bf3-d6f140383805>
2005-02-23 00:46:50 +0000

    Documentation now available.

--- web/trunk/docs.php
+++ web/trunk/docs.php
@@ -8,6 +8,33 @@
 
 	open( 'div', array( 'id' => 'body' ) );
 
-	tag( 'p', array(), "Not ready yet..." );
+	$_jMList = txttag( 'span', array( 'class' => 'jmlist' ), 'jMList' );
+
+
+	h2( 'User Documentation' );
+
+	tag( 'p', array(), a( "jMList Manual", 'manual-html/' ) );
+
+	tag( 'p', array(),
+		"This documentation is intended for users of $_jMList. It is focused ".
+		"on how to use this utility."
+	);
+
+
+	h2( 'Developer Documentation' );
+
+	tag( 'p', array(), a( "jMList Developer's Reference", 'dev-manual/html/' ) );
+
+	tag( 'p', array(),
+		"This documentation provides information about all functions, ".
+		"types and enumerations used on $_jMList source code. It is useful ".
+		"only for developers."
+	);
+
+	tag( 'p', array(),
+		"It was generated from the commented source code using ".
+		a( "Doxygen", 'http://www.doxygen.org/' ). "."
+	);
+
 
 ?>
