b9047863d7ccc107cbdb3f59a768a49312ff57f6
juliano <juliano@2fa30b31-ecef-0310-9bf3-d6f140383805>
2005-03-26 04:52:12 +0000

    Screenshots.

--- web/trunk/screenshots.php
+++ web/trunk/screenshots.php
@@ -8,6 +8,39 @@
 
 	open( 'div', array( 'id' => 'body' ) );
 
-	tag( 'p', array(), "Not ready yet..." );
+	h2( 'Live Screenshot' );
+
+	$_jMList = txttag( 'span', array( 'class' => 'jmlist' ), 'jMList' );
+
+	open( 'div', array( 'class' => 'margin' ) );
+	open( 'table', array( 'border' => '0' ) );
+	open( 'tr' );
+
+	tag( 'td', array( 'align' => 'center' ),
+		a(
+			txttag( 'img', array(
+				'border' => 0,
+				'src' => 'demo/rclayderman-thumb.png',
+				'alt' => 'Try it!'
+			) ),
+			'demo/rclayderman.html',
+			'Try it!'
+		)
+	);
+
+	tag( 'td', array( 'valign' => 'top' ),
+		txttag( 'p', array(),
+			"This is an example of output produced by $_jMList. The ".
+			"stylesheet used was xhtml-collapsible, which generates a ".
+			"tree listing in XHTML using JavaScript to make folders ".
+			"collapsible like in a file manager."
+		).
+		txttag( 'p', array(),
+			a( "Here", 'demo/rclayderman.xml' ).
+			" is the unstyled XML output produced by jMList."
+		)
+	);
+
+	close( 'div' );
 
 ?>
